55579e0f3efdacd445997019df101206217e2b83
tlebon <tlebon@gmail.com>
2018-09-04 21:15:05 +0200

    broken still

--- routes/index.js
+++ routes/index.js
@@ -10,8 +10,9 @@ router.get('/', (req, res, next) => {
 
 router.get('/locate/:lat/:lng', (req, res, next) => {
   // res.send(req.params)
-  const url = "https://api.yelp.com/v3/businesses/search?latitude=" + req.params.lat + "&longitude=" + req.params.lng + "&radius=2000&categories=bars&limit=3&sort_by=rating"
-  console.log(typeof url)
+  const bloop = ("https://api.yelp.com/v3/businesses/search?latitude=" + req.params.lat + "&longitude=" + req.params.lng + "&radius=2000&categories=restaurants&limit=3&sort_by=rating");
+  const url = ("https://api.yelp.com/v3/businesses/search?latitude=" + req.params.lat + "&longitude=" + req.params.lng + "&radius=2000&categories=bars&limit=3&sort_by=rating")
+  // console.log(url)
   axios({
     url,
     headers: {
@@ -21,23 +22,24 @@ router.get('/locate/:lat/:lng', (req, res, next) => {
     .then(result => {
       // res.send(result.data.businesses)
       let bars = result.data.businesses
-      res.render('index', { bars })
+      // res.render('index', { bars })
       // $(".results").append(businesses[0].alias)
-    })
-    .then(() => {
-      const urlb = "https://api.yelp.com/v3/businesses/search?latitude=" + req.params.lat + "&longitude=" + req.params.lng + "&radius=2000&categories=restaurants&limit=3&sort_by=rating"
-      return axios.get({
-        urlb,
+      
+      // console.log(urlb)
+      return axios({
+        bloop,
         headers: {
           'Authorization': 'Bearer _qAdbCs6bFfmv-yAFllNXRJtKeO3MZUvjA274v_Fiof_FiKg4Uchv6JpNbTi3EUWi_wOgMIDIX62gASOOYDmhODB4CdwKrKhtJCrj2QcOkV-q7f1Zgg-B7-FyFqNW3Yx'
         }
       })
+    
     })
+      
     .then(result => {
       // res.send(result.data.businesses)
       let restaurants = result.data.businesses
       console.log(restaurants)
-      res.render('index', { restaurants })
+      res.render('index', {bars},{ restaurants })
       // $(".results").append(businesses[0].alias)
     })
     .catch(err => {
