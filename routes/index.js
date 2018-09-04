197d1f7909aebb74c49ab373e953485e85766029
tlebon <tlebon@gmail.com>
2018-09-04 19:25:24 +0200

    added restaurant axios calls, still needs work, saving and then branching to work on axios.all

--- routes/index.js
+++ routes/index.js
@@ -9,21 +9,39 @@ router.get('/', (req, res, next) => {
 });
 
 router.get('/locate/:lat/:lng', (req, res, next) => {
-  console.log(req.params)
   // res.send(req.params)
-    const url = "https://api.yelp.com/v3/businesses/search?latitude=" + req.params.lat + "&longitude=" + req.params.lng + "&radius=2000&categories=bars&limit=3&sort_by=rating"
-    axios({
-      url,
-      headers: {
-        'Authorization': 'Bearer _qAdbCs6bFfmv-yAFllNXRJtKeO3MZUvjA274v_Fiof_FiKg4Uchv6JpNbTi3EUWi_wOgMIDIX62gASOOYDmhODB4CdwKrKhtJCrj2QcOkV-q7f1Zgg-B7-FyFqNW3Yx'
-      }
-    })
+  const url = "https://api.yelp.com/v3/businesses/search?latitude=" + req.params.lat + "&longitude=" + req.params.lng + "&radius=2000&categories=bars&limit=3&sort_by=rating"
+  console.log(typeof url)
+  axios({
+    url,
+    headers: {
+      'Authorization': 'Bearer _qAdbCs6bFfmv-yAFllNXRJtKeO3MZUvjA274v_Fiof_FiKg4Uchv6JpNbTi3EUWi_wOgMIDIX62gASOOYDmhODB4CdwKrKhtJCrj2QcOkV-q7f1Zgg-B7-FyFqNW3Yx'
+    }
+  })
     .then(result => {
       // res.send(result.data.businesses)
       let bars = result.data.businesses
-      console.log(bars)
-      res.render('index',{bars})
+      res.render('index', { bars })
       // $(".results").append(businesses[0].alias)
+    })
+    .then(() => {
+      const urlb = "https://api.yelp.com/v3/businesses/search?latitude=" + req.params.lat + "&longitude=" + req.params.lng + "&radius=2000&categories=restaurants&limit=3&sort_by=rating"
+      return axios.get({
+        urlb,
+        headers: {
+          'Authorization': 'Bearer _qAdbCs6bFfmv-yAFllNXRJtKeO3MZUvjA274v_Fiof_FiKg4Uchv6JpNbTi3EUWi_wOgMIDIX62gASOOYDmhODB4CdwKrKhtJCrj2QcOkV-q7f1Zgg-B7-FyFqNW3Yx'
+        }
+      })
+    })
+    .then(result => {
+      // res.send(result.data.businesses)
+      let restaurants = result.data.businesses
+      console.log(restaurants)
+      res.render('index', { restaurants })
+      // $(".results").append(businesses[0].alias)
+    })
+    .catch(err => {
+      console.log(err)
     })
 });
 
