d21adb57b731a116835331a5fe32493ed535882e
tlebon <tlebon@gmail.com>
2018-09-04 16:54:33 +0200

    button finally pulls yelp api data

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,11 +1,11 @@
 $(document).ready(() => {
   console.log("ready")
 
+  getResults();
 
-  $('button.random').on('click', function () {
-    console.log('getting results')
-    getResults();
-  })
+  // $('button.random').on('click', function () {
+  //   console.log('getting results')
+  // })
   function getResults() {
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(function (position) {
@@ -15,7 +15,10 @@ $(document).ready(() => {
           lng: position.coords.longitude
         };
         console.log('center: ', center)
-        
+        $(".redirect").html( '<a href="locate/'+center.lat+'/'+center.lng+'"><button class="random">Haben Sie einen schönen Tag</button></a>')
+        // $(".lat").val(center.lat)
+        // $(".lng").val(center.lng)
+        console.log("inserted link")
       }, function () {
         console.log('Error in the geolocation service.');
       });
--- routes/index.js
+++ routes/index.js
@@ -9,16 +9,19 @@ router.get('/', (req, res, next) => {
 });
 
 router.get('/locate/:lat/:lng', (req, res, next) => {
-  const url = "https://api.yelp.com/v3/businesses/search?latitude=" + lat + "&longitude=" + lng + "&radius=2000"
-  axios({
-    url,
-    headers: {
-      'Authorization': 'Bearer _qAdbCs6bFfmv-yAFllNXRJtKeO3MZUvjA274v_Fiof_FiKg4Uchv6JpNbTi3EUWi_wOgMIDIX62gASOOYDmhODB4CdwKrKhtJCrj2QcOkV-q7f1Zgg-B7-FyFqNW3Yx'
-    }
-  })
+  console.log(req.params)
+  // res.send(req.params)
+    const url = "https://api.yelp.com/v3/businesses/search?latitude=" + req.params.lat + "&longitude=" + req.params.lng + "&radius=2000"
+    axios({
+      url,
+      headers: {
+        'Authorization': 'Bearer _qAdbCs6bFfmv-yAFllNXRJtKeO3MZUvjA274v_Fiof_FiKg4Uchv6JpNbTi3EUWi_wOgMIDIX62gASOOYDmhODB4CdwKrKhtJCrj2QcOkV-q7f1Zgg-B7-FyFqNW3Yx'
+      }
+    })
     .then(result => {
-      console.log(result)
-      $(".results").append(businesses[0].alias)
+      res.send(result.data)
+      // res.render('index',result.data)
+      // $(".results").append(businesses[0].alias)
     })
 });
 
