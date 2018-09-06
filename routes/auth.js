d825d50f6a35ca16136c51ac2bfa4acd3cb8eb8a
gitgut0110 <jsimms0110@gmail.com>
2018-09-06 19:11:15 +0200

    disabled yelp

--- routes/auth.js
+++ routes/auth.js
@@ -200,12 +200,12 @@ function checkRoles(role) {
   };
 }
 
-require('../utils/YelpEventUpdater-copy')
+// require('../utils/YelpEventUpdater-copy')
 
-router.get("/getcoords", (req, res) => {
-  res.yelpIt()
-  console.log(`yelpIt activated`)
-});
+// router.get("/getcoords", (req, res) => {
+//   res.yelpIt()
+//   console.log(`yelpIt activated`)
+// });
 
 
 module.exports = router;
