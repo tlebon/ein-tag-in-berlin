d9075a272eafeab4c4e9ca2bc3beaa29998cf7a8
gitgut0110 <jsimms0110@gmail.com>
2018-09-11 17:18:42 +0200

    automateYelpEvents

--- routes/auth.js
+++ routes/auth.js
@@ -205,7 +205,7 @@ const yelp = require('../routes/YelpEventUpdater-copy')
 
 router.get("/getcoords", (req, res) => {
   console.log(yelp)
-  yelp()
+  let autoYelp = setInterval(yelp, 300000)
   console.log(`yelpIt activated`)
 });
 
